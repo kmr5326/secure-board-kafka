527f81b960b17df3880f37b911f8386c4b05ac48
kmr5326 <kmr5326@naver.com>
2025-05-20 23:33:04 +0900

    jpa entity 생성 확인용

---
+++ backend/springSecurityStudy/src/main/java/com/hg/springSecurityStudy/model/SampleEntity.java
@@ -0,0 +1,20 @@
+package com.hg.springSecurityStudy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SampleEntity {
+    @Id
+    private Integer pk;
+
+    private String data;
+}
