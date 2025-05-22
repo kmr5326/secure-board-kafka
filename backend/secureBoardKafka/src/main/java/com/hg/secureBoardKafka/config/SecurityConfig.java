72ffedf09ed7e440de567b8595f452433eca1cd9
kmr5326 <kmr5326@naver.com>
2025-05-22 23:15:36 +0900

    SecurityConfig 기본 설정 작성

--- backend/secureBoardKafka/src/main/java/com/hg/secureBoardKafka/config/SecurityConfig.java
+++ backend/secureBoardKafka/src/main/java/com/hg/secureBoardKafka/config/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,12 +18,21 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults())
+                .csrf((csrf)->csrf.disable())
+                .authorizeHttpRequests((authorize)-> authorize.anyRequest().permitAll())
+                .sessionManagement((session)->session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(formLogin-> formLogin.disable())
+                .logout(logout -> logout
+                        .logoutUrl("/api/auth/logout"))
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().authenticated()
                 )
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
